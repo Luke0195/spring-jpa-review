965e844d7f44667e5bdedbf4a6dde580f112d4ed
Lucas Santos <lucas201273@gmail.com>
2024-07-28 06:24:32 -0300

    initial commit

---
+++ src/test/java/br/com/boostore/jpa/JpaApplicationTests.java
@@ -0,0 +1,19 @@
+package br.com.boostore.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import br.com.bookstore.jpa.JpaApplication;
+
+@SpringBootTest
+@SpringBootConfiguration
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		JpaApplication.main(new String[]{});
+	}
+
+}
